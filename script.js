9ab6697aea47ff18daaef13c6aae30846d276aab
ezy05 <88734153+ezy05@users.noreply.github.com>
2025-02-01 18:19:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("menu-toggle").addEventListener("click", function () {
+  var menu = document.querySelector("nav ul");
+  menu.classList.toggle("active"); // Adaugă sau elimină clasa "active"
+});
